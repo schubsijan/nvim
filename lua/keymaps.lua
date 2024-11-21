804c77d86b2a64e8b66bb71252b328888f46ea47
schubsijan <arcusamicus@gmail.com>
2024-11-22 00:27:15 +0100

    keymaps angepasst

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -6,7 +6,7 @@
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>lq', vim.diagnostic.setloclist, { desc = '[L]ist diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -16,11 +16,11 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+-- Disable arrow keys in normal mode
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
@@ -44,5 +44,31 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+-- im normalen Modus soll strg+a alles Markieren
+vim.keymap.set('n', '<c-a>', 'ggVG')
+
+-- leader-key+{q|w} soll das selbe manchen wie `:q` und `:w`
+vim.keymap.set('n', '<leader>q', '<cmd>q<cr>', { desc = 'quit NeoVim' })
+vim.keymap.set('n', '<leader>w', '<cmd>w<cr>', { desc = 'save current File' })
+
+-- tippen von "ii" im insert-Modus hat den selben Effect, wie Ecape drücken (verlässt also den instert-Modus)
+vim.keymap.set('i', 'ii', '<Esc>')
+
+-- wenn man im Visual-Mode Zeilen gehighlightet hat kann man sie mit J oder K nach oben bzw. unter verschieben
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- wenn man in Normal-Mode über einem Wort ist und <leader>rw drückt, dann kommt man in ein einfaches Menu, dass alle Wörter dieser Art im Filer ersetzt
+vim.keymap.set('n', '<leader>rw', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = 'replace all instances of word under cursor' })
+
+-- <leader>y & <leader>yy um ins System-Clipboard zu kopieren sonst nur innerhalb von Vim
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]], { desc = 'In System clipboard kopieren' })
+vim.keymap.set('n', '<leader>yy', [["+Y]], { desc = 'Zeile in System clipboard kopieren' })
+-- <leader>m um das dannachfolgende (meist d) nicht in clipboard von vim zu packen, sondern einfach nur zu löschen
+vim.keymap.set({ 'n', 'v' }, '<leader>m', [["_d]], { desc = 'Löschung nicht in clipboard' })
+
+-- wenn <lead>p dann zwar pasten aber das gehighlighte Wort wird nicht in den buffer gepackt
+vim.keymap.set('x', '<leader>p', '"_dP', { desc = 'Pasten, aber gehighlightetes Wort nicht in buffer' })
 
 -- vim: ts=2 sts=2 sw=2 et
