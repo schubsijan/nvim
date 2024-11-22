5f743e4ecc61187ea0277785af76b03ac5903441
schubsijan <arcusamicus@gmail.com>
2024-11-22 18:31:07 +0100

    todo-comments angepasst

--- lua/kickstart/plugins/todo-comments.lua
+++ lua/kickstart/plugins/todo-comments.lua
@@ -1,5 +1,20 @@
 -- Highlight todo, notes, etc in comments
 return {
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('todo-comments').setup {
+        highlight = {
+          pattern = [[.*<(KEYWORDS)\s*]], -- pattern or table of patterns, used for highlighting (vim regex)
+        },
+        search = {
+          pattern = [[\b(KEYWORDS)\b]], -- ripgrep regex
+        },
+      }
+      vim.keymap.set('n', '<leader>lt', '<cmd>TodoTelescope<CR>', { desc = 'List all Tasks in project' })
+    end,
+  },
 }
 -- vim: ts=2 sts=2 sw=2 et
